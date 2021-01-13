de533990ee590be3d4abdf736fd2c89ecebacffa
Jordan-Maynard101 <77409265+Jordan-Maynard101@users.noreply.github.com>
2021-01-13 14:42:55 -0500

    Update code.js

--- code.js
+++ code.js
@@ -2,15 +2,20 @@ var word = "bottles";
 var count = 99;
 while (count > 0) {
 	document.write(count + " " + word + " of soda on the wall");
+	document.write("<br>");
 	document.write(count + " " + word + " of soda,");
+	document.write("<br>");
 	document.write("Take one down, pass it around,");
+	document.write("<br>");
 	count = count - 1;
 		if (count > 0) 
 			{
 				document.write(count + " " + word + " of soda on the wall.");
+				document.write("<br>");
 			} 
 		else 
 			{
 				document.write("No more " + word + " of soda on the wall.");
+				document.write("<br>");
 			}
  }
