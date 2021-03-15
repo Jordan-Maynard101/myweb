f86261328aa5abd5c5605b67c2e32520bbc7a48e
Jordan-Maynard101 <77409265+Jordan-Maynard101@users.noreply.github.com>
2021-03-15 12:33:14 -0400

    Add files via upload

--- fiat.js
+++ fiat.js
@@ -1,31 +1,61 @@
-function makeCar()
+var fiat = 
 {
-	var makes = ["Chevy", "GM", "Fiat", "Webville Motors", "Tucker"];
-	var models = ["Cadillac", "500", "Bel-Air", "Taxi", "Torpedo"];
-	var years = [1955, 1957, 1948, 1954, 1961];
-	var colors = ["red", "blue", "tan", "yellow", "white"];
-	var convertible = [true, false];
-	var rand1 = Math.floor(Math.random() * makes.length);
-	var rand2 = Math.floor(Math.random() * models.length);
-	var rand3 = Math.floor(Math.random() * years.length);
-	var rand4 = Math.floor(Math.random() * colors.length);
-	var rand5 = Math.floor(Math.random() * 5) + 1;
-	var rand6 = Math.floor(Math.random() * 2);
-	var car = 
+	make: "Fiat",
+	model: "500",
+	year: 1957,
+	color: "Medium Blue",
+	passengers: 2,
+	convertible: false,
+	mileage: 88000,
+	fuel: 0,
+	started: false,
+ 
+	start: function() 
 	{
-		make: makes[rand1],
-		model: models[rand2],
-		year: years[rand3],
-		color: colors[rand4],
-		passengers: rand5,
-		convertible: convertible[rand6],
-		mileage: 0
-	};
-	return car;
- }
- function displayCar(car) 
- {
-	 console.log("Your new car is a " + car.year + " " + car.make + " " + car.model);
- }
- var carToSell = makeCar();
- displayCar(carToSell);
+		if (this.fuel == 0) 
+		{
+			alert("The car is on empty, fill up before starting!");
+		} 
+		else 
+		{
+			this.started = true;
+		}
+	},
+	stop: function()
+	{
+		this.started = false;
+	},
+	drive: function()
+	{
+		if (this.started)
+		{
+			if (this.fuel > 0)
+			{
+				alert(this.make + " " +
+					this.model + " goes zoom zoom!");
+				this.fuel = this.fuel - 1;
+			}
+			else 
+			{
+				alert("Uh oh, out of fuel.");
+				this.stop();
+			}
+		} 
+		else 
+		{
+			alert("You need to start the engine first.");
+		}
+	},
+	addFuel: function(amount)
+	{
+		this.fuel = this.fuel + amount;
+	}
+}
+fiat.start();
+fiat.drive();
+fiat.addFuel(2);
+fiat.start();
+fiat.drive();
+fiat.drive();
+fiat.drive();
+fiat.stop();
